f171b29c15c0df1b02f4d1026278bc538194f94e
simvolice <simvolice@gmail.com>
2017-03-16 21:27:19 +0600

    Выгрузка улиц из memory

--- routes/streetRoter.js
+++ routes/streetRoter.js
@@ -7,16 +7,22 @@ const express = require('express');
 const router = express.Router();
 const Streets = require('../services/StreetService');
 
-router.get('/getstreets', function(req, res, next){
+let StreetsAstana = [];
+
+Streets.getStreetsAstana().then(function (result) {
+
+    StreetsAstana = result;
+
+});
 
 
+router.post('/getstreets', function(req, res, next){
 
-    Streets.getStreetsAstana().then(function (result) {
 
-        res.json({"code": "ok", "resultFromDb": result});
+    res.json({"code": "ok", "resultFromDb": StreetsAstana});
 
-    });
 
+    StreetsAstana = [];
 
 
 });
